7ff563949f49f08a22dc39856835dc756b5bf886
= <onedavid.nicholas@gmail.com>
2020-08-08 17:26:59 +0300

    Initial Commit

---
+++ profiles/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from profiles.models import UserProfile
+from django.conf import settings
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserProfile
+        fields = ('id', 'name', 'email', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        user = settings.AUTH_USER_MODEL.objects.create_user(
+            email = validated_data['email'],
+            name = validated_data['name'],
+            password = validated_data['password']
+        )
+        return user
+
