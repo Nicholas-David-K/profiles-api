aa859569b49793a46c2465886775373ad1171511
= <onedavid.nicholas@gmail.com>
2020-08-09 11:03:05 +0300

    Added SearchFilter

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
+
 from profiles.models import UserProfile
+
 from profiles.api.serializers import UserProfileSerializer
 
 from rest_framework.permissions import IsAuthenticated
+
 from profiles.api.permissions import IsProfileOwnerOrReadOnly
+
+from rest_framework.filters import SearchFilter
 
 
 
@@ -11,3 +16,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated, IsProfileOwnerOrReadOnly]
+    filter_backends = [ SearchFilter ]
+    search_fields = ['name', 'email']
