8758b1d2eb1cfce9e59e2b0ac310f4cc02d14a73
Nicholas David Kamau <69382479+Nicholas-David-K@users.noreply.github.com>
2020-12-17 10:38:51 +0300

    Update admin.py

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from profiles.models import UserProfile
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext as _
 
 # Register your models here.
-admin.site.register(UserProfile)
+class AdminUser(UserAdmin):
+  
+   ordering = ['id']
+   list_display = ['first_name', 'last_name', 'email']
+   fieldsets = (
+     (None, {'fields': ('email', 'password')}),
+     (_('personal Info'), {'fields': ('first_name', 'last_name')}),
+     (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+     (_('Inportant dates'), {'fields': ('last_login',)})
+   )
+   add_fieldsets = (
+     (None, {'classes': ('wide',), 'fields': ('first_name', 'last_name', 'email', 'password1', 'password2')}),
+   )
+
+admin.site.register(UserProfile, AdminUser)
